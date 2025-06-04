3864dacce5ae0640cefa726a25d8b8510b9b0241
nick1024working <nick1024working@gmail.com>
2025-06-04 10:53:36 +0800

    統一連接字串

--- SpecialTopic/Properties/Settings.Designer.cs
+++ SpecialTopic/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SpecialTopic.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace SpecialTopic.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=TeamA_Project;Integrated Security=True;Encrypt=True" +
-            ";TrustServerCertificate=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=TeamA_Project;Integrated Security=True;TrustServerC" +
+            "ertificate=True")]
         public string TeamA_ProjectConnectionString {
             get {
                 return ((string)(this["TeamA_ProjectConnectionString"]));
