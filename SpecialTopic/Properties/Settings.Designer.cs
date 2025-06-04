8a47cac1f1112b380bd0285b62dcf86b1864abbc
nick1024working <nick1024working@gmail.com>
2025-06-04 11:56:10 +0800

    統一連接字串

--- SpecialTopic/Properties/Settings.Designer.cs
+++ SpecialTopic/Properties/Settings.Designer.cs
@@ -26,8 +26,7 @@ namespace SpecialTopic.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=TeamA_Project;User ID=sa;Password=sa0529;TrustServe" +
-            "rCertificate=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=TeamA_Project;Integrated Security=True")]
         public string TeamA_ProjectConnectionString {
             get {
                 return ((string)(this["TeamA_ProjectConnectionString"]));
